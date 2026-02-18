03ab01deb7e1e64e8934acafd6f3eccc85ed7113
e <erink@unca.edu>
2026-02-18 15:15:28 -0500

    lecture09

---
+++ lectures/lecture09/02-style-property-demo/main.js
@@ -0,0 +1,32 @@
+const changeElementStyle = (newColor, selector) => {
+    // your code here...
+    let bodyEL = document.querySelector(selector);
+    bodyEL.style.backgroundColor = newColor;
+    bodyEL.style.fontSize = "25px";
+
+};
+
+// const makeRed = () => {
+//     // your code here...
+//     console.log('Change background to red');
+//     document.querySelector('body').style.backgroundColor = '#821717';
+// };
+
+// const makeBlue = () => {
+//     // your code here...
+//     console.log('Change background to blue');
+//     document.querySelector('body').style.backgroundColor = '#4191bf'
+// };
+
+// const makePink = () => {
+//     // your code here...
+//     console.log('Change background to pink');
+//     document.querySelector('body').style.backgroundColor = 'pink'
+// };
+
+// const makeOrange = () => {
+//     // your code here...
+//     console.log('Change background to orange');
+//     document.querySelector('body').style.backgroundColor = '#ed7124'
+// };
+
