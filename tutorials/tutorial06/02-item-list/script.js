eca280f5d573924432d0b6e30d6bf1504b5f6ae2
e <erink@unca.edu>
2026-02-25 14:35:46 -0500

    tutorial06-02

---
+++ tutorials/tutorial06/02-item-list/script.js
@@ -0,0 +1,18 @@
+const items = ['Apple', 'Banana', 'Orange', 'Grape', 'Mango'];
+
+const itemList = document.querySelector('#itemList');
+
+console.log(items);
+console.log(itemList);
+
+function displayItems() {
+  itemList.innerHTML = '';
+
+  for (let i = 0; i < items.length; i++) {
+    const listItemHTML = `<li>${items[i]}</li>`;
+
+    itemList.insertAdjacentHTML('beforeend', listItemHTML);
+  }
+}
+
+displayItems();
