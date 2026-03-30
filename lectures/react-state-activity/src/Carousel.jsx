a92f8fae5af8bf54e79c3c143770dc4708b6655d
e <erink@unca.edu>
2026-03-30 15:44:19 -0400

    react activity

--- lectures/react-state-activity/src/Carousel.jsx
+++ lectures/react-state-activity/src/Carousel.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Carousel.css";
 
-export default function Carousel({ gallery }) {
-    console.log(gallery);
+export default function Carousel({ photos }) {
+    const [index, setIndex] = useState(0);
+    console.log(photos);
+
+    function next() {
+        console.log("Next");
+        if (index === photos.length - 1) {
+            setIndex(0);
+        } else {
+            setIndex(index + 1);
+        }
+    }
+
+    function previous() {
+        console.log("Previous");
+        if (index === 0) {
+            setIndex(photos.length - 1);
+        } else {
+            setIndex(index - 1);
+        }
+    }
+
     return (
         <div className="carousel">
             {/* display the first image in the gallery array below */}
+            <img src={photos[index]} />
             {/* also display a "Photo X of Y" message below the image */}
+            <p>Showing photo {index + 1} of {photos.length}.</p>
+            <button onClick={previous}>Previous</button>
+            <button onClick={next}>Next</button>
         </div>
     );
 }
---
+++ lectures/react-state-activity/src/Galleries.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+export default function Galleries() {
+    return (
+        <button>{galleries[0].name}</button>
+    )
+}
