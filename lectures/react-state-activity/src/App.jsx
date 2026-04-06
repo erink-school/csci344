b391bfc439c1bc9d69f674425fb695312bd4f4c7
e <erink@unca.edu>
2026-04-06 14:06:17 -0400

    react-state-activity

--- lectures/react-state-activity/src/App.jsx
+++ lectures/react-state-activity/src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Carousel from "./Carousel";
 import Gaalleries from "./Galleries";
 import "./App.css";
@@ -6,32 +6,41 @@ import Galleries from "./Galleries";
 
 export default function App() {
     let [activeIndex, setActiveIndex] = useState(0);
+    let  [photos, setPhotos] = useState([]);
+
+    async function getFlickrPhotos() {
+      const url = galleries[activeIndex].endpoint;
+      const response = await fetch(url);
+      const arrOfObjects = await response.json();
+      const arrOfStrings = arrOfObjects.map(obj => obj.img_url);
+      console.log(arrOfStrings);
+      setPhotos(arrOfStrings);
+    }
+
+    useEffect(() => {
+      getFlickrPhotos();
+    }, [activeIndex])
 
     const galleries = [
       {
-        name: "Nature",
-        photos: [
-          "https://picsum.photos/id/164/600/430",
-          "https://picsum.photos/id/116/600/430",
-          "https://picsum.photos/id/182/600/430"
-        ]
+        name: "Cats",
+        endpoint: "https://www.apitutor.org/flickr/simple/?tags=cat"
       },
       {
-        name: "City",
-        photos: [
-          "https://picsum.photos/id/127/600/430",
-          "https://picsum.photos/id/140/600/430",
-          "https://picsum.photos/id/141/600/430"
-        ]
+        name: "Dogs",
+        endpoint: "https://www.apitutor.org/flickr/simple/?tags=dog"
       },
       {
-        name: "Animals",
-        photos: [
-          "https://picsum.photos/id/122/600/430",
-          "https://picsum.photos/id/176/600/430",
-          "https://picsum.photos/id/196/600/430",
-          "https://picsum.photos/id/190/600/430"
-        ]
+        name: "Asheville",
+        endpoint: "https://www.apitutor.org/flickr/simple/?tags=asheville"
+      },
+      {
+        name: "Airplanes",
+        endpoint: "https://www.apitutor.org/flickr/simple/?tags=airplane"
+      },
+      {
+        name: "Space",
+        endpoint: "https://www.apitutor.org/flickr/simple/?tags=space"
       }
     ];
 
@@ -43,7 +52,7 @@ export default function App() {
               activeIndex={activeIndex}
               setActiveIndex={setActiveIndex}
             />
-            <Carousel photos={galleries[activeIndex].photos} />
+            <Carousel photos={photos} />
         </div>
     );
 }
